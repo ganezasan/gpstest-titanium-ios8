c748fcdd24bed06bd5b0c858f012188e72293658
manager <investi0173gate@yahoo.co.jp>
2014-10-09 18:30:07 +0900

    init

---
+++ app/controllers/index.js
@@ -0,0 +1,9 @@
+Ti.Geolocation.addEventListener('authorization',function(e){
+      Ti.API.info('authorization event:' + JSON.stringify(e));
+});
+
+// function doClick(e) {
+//     alert($.label.text);
+// }
+
+$.index.open();
